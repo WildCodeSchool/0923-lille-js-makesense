1f1fa3fb67b2e203793fac568835e48e49d62508
VincentRssx <Vincent.rssx59@gmail.com>
2023-12-21 09:59:54 +0100

    ajout des tables dans shema et rename de la branche

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,66 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
-);
+CREATE table user (
+user_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+lastname VARCHAR(100) NOT NULL,
+firstname VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL,
+location VARCHAR(100) NOT NULL,
+picture IMAGE,
+role VARCHAR(100)
+)
+
+CREATE table authentification (
+  authentification_id  INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  password TEXT NOT NULL,
+  date DATE NOT NULL,
+  hour TIME NOT NULL,
+  FOREIGN KEY (user_id) 
+)
+
+
+CREATE table decide_maker(
+decide_maker_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+FOREIGN KEY (user_id)
+)
+
+CREATE table employee(
+  employee_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  FOREIGN KEY (user_id)
+)
+
+CREATE table decision(
+  decision_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  date DATE NOT NULL,
+  status VARCHAR(100) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  FOREIGN KEY (decide_maker_id),
+)
+
+CREATE table assignement(
+  assignement_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  FOREIGN KEY (decide_maker_id),
+  FOREIGN KEY (employee_id),
+  FOREIGN KEY (decision_id)
+)
+
+
+CREATE table admin (
+  admin_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  FOREIGN KEY (user_id)
+)
+
+
+CREATE table comment (
+comment_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+date_time DATETIME NOT NULL,
+message VARCHAR(100) NOT NULL,
+FOREIGN KEY (employee_id),
+FOREIGN KEY (admin_id),
+FOREIGN KEY (decision_id)
+)
+
+CREATE table paragraph(
+  paragraph_id PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  contains TEXT NOT NULL,
+  FOREIGN KEY (decision_id)
+)
