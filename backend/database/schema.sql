0d7177f7339cda54938f8477f42c9a4ec542d3b5
VincentRssx <Vincent.rssx59@gmail.com>
2023-12-22 17:05:58 +0100

    changement decide en decision et ajout de varchar augmentation de leur taille de texte autorisé

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,32 +1,31 @@
 CREATE table user (
-user_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-lastname VARCHAR(100) NOT NULL,
-firstname VARCHAR(100) NOT NULL,
-email VARCHAR(100) NOT NULL,
-location VARCHAR(100) NOT NULL,
-picture VARCHAR(100),
-role VARCHAR(100)
+  user_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  lastname VARCHAR(100) NOT NULL,
+  firstname VARCHAR(100) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  location VARCHAR(255) NOT NULL,
+  picture VARCHAR(255),
+  role VARCHAR(100)
 )
 
 CREATE table authentification (
   authentification_id  INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  password TEXT NOT NULL,
-  date DATE NOT NULL,
-  hour TIME NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  datetime DATETIME NOT NULL,
   FOREIGN KEY (user_id) REFERENCES user(user_id)
 )
 
 
-CREATE table decide_maker(
-decide_maker_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-user_id INT,
+CREATE table decision_maker(
+  decision_maker_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  user_id INT,
   FOREIGN KEY (user_id) REFERENCES user(user_id)
 )
 
 CREATE table employee(
   employee_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
 )
 
 CREATE table decision(
@@ -34,37 +33,37 @@ CREATE table decision(
   date DATE NOT NULL,
   status VARCHAR(100) NOT NULL,
   title VARCHAR(100) NOT NULL,
-  decide_maker_id INT,
-  FOREIGN KEY (decide_maker_id) REFERENCES decide_maker(decide_maker_id)
+  decision_maker_id INT,
+  FOREIGN KEY (decision_maker_id) REFERENCES decision_maker(decision_maker_id)
 )
 
 CREATE table assignement(
   assignement_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  decide_maker_id INT,
+  decision_maker_id INT,
   employee_id INT,
   decision_id INT,
-  FOREIGN KEY (decide_maker_id) REFERENCES decide_maker(decide_maker_id),
+  FOREIGN KEY (decision_maker_id) REFERENCES decision_maker(decision_maker_id),
   FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
   FOREIGN KEY (decision_id) REFERENCES decision(decision_id)
 )
 
 
-CREATE table admin (
+CREATE table admin(
   admin_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(user_id) 
+  FOREIGN KEY (user_id) REFERENCES user(user_id) 
 )
 
 CREATE table comment (
-comment_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-date_time DATETIME NOT NULL,
-message VARCHAR(100) NOT NULL,
-employee_id INT,
-admin_id INT,
-decision_id INT,
-FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
-FOREIGN KEY (admin_id) REFERENCES admin(admin_id),
-FOREIGN KEY (decision_id) REFERENCES decision(decision_id)
+  comment_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  date_time DATETIME NOT NULL,
+  message TEXT NOT NULL,
+  employee_id INT,
+  admin_id INT,
+  decision_id INT,
+  FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+  FOREIGN KEY (admin_id) REFERENCES admin(admin_id),
+  FOREIGN KEY (decision_id) REFERENCES decision(decision_id)
 )
 
 CREATE table paragraph(
