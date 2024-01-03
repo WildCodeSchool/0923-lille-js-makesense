6269c61a40c25da18deb2e9fc984b4f4a7e0ebff
Farid-Zian <130969864+Farid-Zian@users.noreply.github.com>
2024-01-03 14:07:01 +0100

    bind buttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import App from "./App";
+import NewUsers from "./components/NewUsers/NewUsers";
 /* import Login from "./pages/Login/Login"; */
 import Homepage from "./pages/Homepage/Homepage";
 /* 
 import Decision from "./pages/Decision/Decision"
 import CreateDecision from "./pages/CreateDecision/CreateDecision";
-import NewUsers from ".pages/NewUsers/NewUsers";
- */
+*/
 
 const router = createBrowserRouter([
   {
@@ -18,12 +17,17 @@ const router = createBrowserRouter([
     /* element: <Login /> */
   },
   {
+    path: "/newuser",
+    element: <NewUsers />,
+  },
+  {
     element: <App />,
     children: [
       {
         path: "/homepage",
         element: <Homepage />,
       },
+
       /*  
       {
         path: "/decision",
@@ -32,10 +36,6 @@ const router = createBrowserRouter([
       {
         path: "/createdecision",
         element: <CreateDecision />,
-      },
-          {
-        path: "/newuser",
-        element: <NewUsers />,
       },
       */
     ],
