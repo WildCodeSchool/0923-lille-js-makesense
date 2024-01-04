36138a53e5012514da66d559c688e97c9e1e6ae4
VincentRssx <Vincent.rssx59@gmail.com>
2024-01-04 17:08:40 +0100

    modification navbar for position fixed

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./Nav.scss";
 
 function Nav() {
+  const [edit, setEdit] = useState(false);
+
+  const handleclick = () => {
+    setEdit(!edit);
+  };
+
   return (
     <nav className="nav__nav">
       <a href="*" className="nav__logo">
@@ -27,7 +34,27 @@ function Nav() {
           className="nav__avatar"
           alt="user avatar"
           src="https://placehold.co/600x400"
+          onClick={handleclick}
+          role="presentation"
         />
+        {edit ? (
+          <section className="profil_avatar">
+            <ul>
+              <li>Firstname</li>
+              <li>Lastname</li>
+              <li>Mail</li>
+              <li>Location</li>
+              <li>Position</li>
+              <li>?</li>
+              <li>Log out</li>
+            </ul>
+          </section>
+        ) : null}
+        {/* <img
+          className="edit_avatar"
+          alt="edit_avatar"
+          src="../../assets/image/user-pen.png"
+        /> */}
       </ul>
     </nav>
   );
