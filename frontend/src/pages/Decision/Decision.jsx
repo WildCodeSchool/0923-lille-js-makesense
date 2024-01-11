f6f7fad3fa775691acc1fe66510f120bfe681d2d
Helene Rampelberg <h.rampelberg@yahoo.fr>
2024-01-11 16:30:56 +0100

    add media queries on decision page and its components

--- frontend/src/pages/Decision/Decision.jsx
+++ frontend/src/pages/Decision/Decision.jsx
@@ -1,28 +1,35 @@
 import "./Decision.scss";
+import { useState } from "react";
 import DescriptionBox from "../../components/DescriptionBox/DescriptionBox";
 import CommentSection from "../../components/CommentSection/CommentSection";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
 
 function Decision() {
+  const [writeComment, setWriteComment] = useState();
   return (
     <main className="decision__page">
       <header className="decision__page--header">
-        <h2 className="openAndClose--date">
+        <h2 className="openAndClose__date">
           Date d'ouverture: 00/00/2023 <br />
           Date de cloture: 00/00/2024
         </h2>
         <ProgressBar />
       </header>
       <section className="decision__page--body">
-        <section className="left__section">
+        <section className={`left__section ${writeComment ? "hidden" : null}`}>
           <DescriptionBox />
         </section>
-        <button type="button" className="commentLink__button">
-          commentaires
-        </button>
-        <section className="right__section">
+        <section
+          className={`right__section ${!writeComment ? "hidden" : null}`}
+        >
           <CommentSection />
         </section>
+        <input
+          value={writeComment ? "Voir la décision" : "Voir les commentaires"}
+          onClick={() => setWriteComment(!writeComment)}
+          type="button"
+          className="commentLink__button"
+        />
       </section>
     </main>
   );
