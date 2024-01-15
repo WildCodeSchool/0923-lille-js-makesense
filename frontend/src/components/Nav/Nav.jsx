3352f3118b03093fda45a5248b72902edb81aa83
VincentRssx <Vincent.rssx59@gmail.com>
2024-01-15 14:35:30 +0100

    my profil complete

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -4,12 +4,8 @@ import "./Nav.scss";
 
 function Nav() {
   const [edit, setEdit] = useState(false);
-  const [help, setHelp] = useState(false);
 
-  const handleMove = () => {
-    setHelp(!help);
-  };
-  const handleclick = () => {
+  const handleMovebubble = () => {
     setEdit(!edit);
   };
 
@@ -43,37 +39,39 @@ function Nav() {
           className="nav__avatar"
           alt="user avatar"
           src="https://placehold.co/600x400"
-          onClick={handleclick}
+          onMouseEnter={handleMovebubble}
           role="presentation"
         />
-        {edit ? (
-          <>
-            <aside className="buble_profil" />
-            <section className="buble_avatar">
-              <ul className="buble_list">
-                <li>Firstname</li>
-                <li>Lastname</li>
-                <li>Mail</li>
-                <li>Location</li>
-                <li>Position</li>
-                <li
-                  onMouseEnter={handleMove}
-                  onMouseLeave={handleMove}
-                  className="buble_hover"
+      </ul>
+      {edit && (
+        <>
+          <aside className="bubble_profil" />
+          <section className="bubble_avatar" onMouseLeave={handleMovebubble}>
+            <ul className="bubble_list">
+              <li>Prénom</li>
+              <li>Nom</li>
+              <li>Email</li>
+              <li>Lieu de travail</li>
+              <li>Poste</li>
+              <li className="bubble_hover">
+                <a
+                  title="Une erreur ? Contactez l'administrateur."
+                  href="mailto:vincent.rssx59@gmail.com"
+                  onClick={handleMovebubble}
                 >
                   ?
-                </li>
-                {help ? (
-                  <p className="buble_help">
-                    Une erreur ? Contactez l'administrateur.
-                  </p>
-                ) : null}
-                <li id="buble_logout">Log out</li>
-              </ul>
-            </section>
-          </>
-        ) : null}
-      </ul>
+                </a>
+              </li>
+              <hr />
+              <li id="bubble_logout">
+                <Link to="/" className="bubble_link">
+                  Se déconnecter
+                </Link>
+              </li>
+            </ul>
+          </section>
+        </>
+      )}
     </nav>
   );
 }
