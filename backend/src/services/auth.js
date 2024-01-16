a32f9d0352ddb9f3aaecc1c5d6c17d1c12979fd0
Florence Buchelet <florence.buchelet@gmail.com>
2024-01-16 15:13:40 +0100

    removed jwt

--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -1,5 +1,4 @@
 const argon2 = require("argon2");
-const jwt = require("jsonwebtoken");
 
 // Options to hash passwords with argon2
 const hashingOptions = {
@@ -23,35 +22,6 @@ const hashPassword = async (req, res, next) => {
   }
 };
 
-const verifyToken = (req, res, next) => {
-  try {
-    // Vérifier la présence de l'en-tête "Authorization" dans la requête
-    const authorizationHeader = req.get("Authorization");
-
-    if (authorizationHeader == null) {
-      throw new Error("Authorization header is missing");
-    }
-
-    // Vérifier que l'en-tête a la forme "Bearer <token>"
-    const [type, token] = authorizationHeader.split(" ");
-
-    if (type !== "Bearer") {
-      throw new Error("Authorization header has not the 'Bearer' type");
-    }
-
-    // Vérifier la validité du token (son authenticité et sa date d'expériation)
-    // En cas de succès, le payload est extrait et décodé
-    req.auth = jwt.verify(token, process.env.APP_SECRET);
-
-    next();
-  } catch (err) {
-    console.error(err);
-
-    res.sendStatus(401);
-  }
-};
-
 module.exports = {
   hashPassword,
-  verifyToken,
 };
