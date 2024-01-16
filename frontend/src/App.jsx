dd3ee5078a57c6b1695feac831997fa63bac246a
Florence Buchelet <florence.buchelet@gmail.com>
2024-01-16 16:32:56 +0100

    contexts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from "react-router-dom";
 import "./App.scss";
+import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
+import { DecisionProvider } from "./contexts/decisionContext";
 
 function App() {
   return (
-    <>
+    <DecisionProvider>
       <Nav />
       <Outlet />
-    </>
+    </DecisionProvider>
   );
 }
 export default App;
