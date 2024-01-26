7fbcd04bc60cb998e6003babe06224b833664501
VincentRssx <Vincent.rssx59@gmail.com>
2024-01-26 17:24:38 +0100

    create delete decision when we are admin

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -67,7 +67,6 @@ CREATE TABLE paragraph (
   paragraph_benefits TEXT,
   paragraph_risks TEXT,
   paragraph_first_decision TEXT,
-  paragraph_decision TEXT,
   paragraph_finale_decision TEXT,
   decision_id INT NOT NULL,
   FOREIGN KEY (decision_id) REFERENCES decision(decision_id)
--- backend/seed.js
+++ backend/seed.js
@@ -234,18 +234,18 @@ const seed = async () => {
     queries.push(
       database.query(
         `INSERT INTO paragraph (paragraph_details, paragraph_impact, paragraph_benefits, paragraph_risks, paragraph_first_decision, paragraph_decision, paragraph_finale_decision, decision_id) VALUES
-        ('Paragraph 1 content', 'Paragraph 2 content', 'Paragraph 3 content', 'Paragraph 4 content', 'Paragraph 49 content', 'Paragraph 50 content', 'Paragraph 56 content', 1),
-        ('Paragraph 5 content', 'Paragraph 6 content', 'Paragraph 7 content', 'Paragraph 8 content', NULL, NULL, NULL, 2),
-        ('Paragraph 9 content', 'Paragraph 10 content', 'Paragraph 11 content', 'Paragraph 12 content', NULL, NULL, NULL, 3),
-        ('Paragraph 13 content', 'Paragraph 14 content', 'Paragraph 15 content', 'Paragraph 16 content', NULL, NULL, NULL, 4),
-        ('Paragraph 17 content', 'Paragraph 18 content', 'Paragraph 19 content', 'Paragraph 20 content', 'Paragraph 52 content', 'Paragraph 51 content', NULL, 5),
-        ('Paragraph 21 content', 'Paragraph 22 content', 'Paragraph 23 content', 'Paragraph 24 content', NULL, NULL, NULL, 6),
-        ('Paragraph 25 content', 'Paragraph 26 content', 'Paragraph 27 content', 'Paragraph 28 content', 'Paragraph 49 content', NULL, 'Paragraph 57 content', 7),
-        ('Paragraph 29 content', 'Paragraph 30 content', 'Paragraph 31 content', 'Paragraph 32 content', 'Paragraph 53 content', 'Paragraph 54 content', 'Paragraph 58 content', 8),
-        ('Paragraph 33 content', 'Paragraph 34 content', 'Paragraph 35 content', 'Paragraph 36 content', NULL, NULL, NULL, 9),
-        ('Paragraph 37 content', 'Paragraph 38 content', 'Paragraph 39 content', 'Paragraph 40 content', 'Paragraph 55 content', 'Paragraph 56 content', 'Paragraph 59 content', 10),
-        ('Paragraph 41 content', 'Paragraph 42 content', 'Paragraph 43 content', 'Paragraph 44 content', NULL, NULL, NULL, 11),
-        ('Paragraph 45 content', 'Paragraph 46 content', 'Paragraph 47 content', 'Paragraph 48 content', NULL, NULL, NULL, 12);`
+        ('Paragraph 1 content', 'Paragraph 2 content', 'Paragraph 3 content', 'Paragraph 4 content', 'Paragraph 49 content', 'Paragraph 56 content', 1),
+          ('Paragraph 5 content', 'Paragraph 6 content', 'Paragraph 7 content', 'Paragraph 8 content', NULL, NULL, 2),
+          ('Paragraph 9 content', 'Paragraph 10 content', 'Paragraph 11 content', 'Paragraph 12 content', NULL, NULL, 3),
+          ('Paragraph 13 content', 'Paragraph 14 content', 'Paragraph 15 content', 'Paragraph 16 content', NULL, NULL, 4),
+          ('Paragraph 17 content', 'Paragraph 18 content', 'Paragraph 19 content', 'Paragraph 20 content', 'Paragraph 51 content', NULL, 5),
+          ('Paragraph 21 content', 'Paragraph 22 content', 'Paragraph 23 content', 'Paragraph 24 content', NULL, NULL, 6),
+          ('Paragraph 25 content', 'Paragraph 26 content', 'Paragraph 27 content', 'Paragraph 28 content', NULL, 'Paragraph 57 content', 7),
+          ('Paragraph 29 content', 'Paragraph 30 content', 'Paragraph 31 content', 'Paragraph 32 content', 'Paragraph 54 content', 'Paragraph 58 content', 8),
+          ('Paragraph 33 content', 'Paragraph 34 content', 'Paragraph 35 content', 'Paragraph 36 content', NULL, NULL, 9),
+          ('Paragraph 37 content', 'Paragraph 38 content', 'Paragraph 39 content', 'Paragraph 40 content', 'Paragraph 56 content', 'Paragraph 59 content', 10),
+          ('Paragraph 41 content', 'Paragraph 42 content', 'Paragraph 43 content', 'Paragraph 44 content', NULL, NULL, 11),
+          ('Paragraph 45 content', 'Paragraph 46 content', 'Paragraph 47 content', 'Paragraph 48 content', NULL, NULL, 12);`
       )
     );
 
